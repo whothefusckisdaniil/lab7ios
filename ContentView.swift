6261aeb8be8704f250e73adc64b4272d748bf344
dautovaaak <dautovaak@gmail.com>
2024-04-07 19:52:17 +0500

    laB7

---
+++ ContentView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State var budget = Budget()
+    
+    var body: some View {
+        TabView {
+            WalletView(budget: $budget)
+                .tabItem {
+                    Label("Wallet", systemImage: "wallet.pass.fill")
+                }
+            ExpensesView(budget: $budget)
+                .tabItem {
+                    Label("Expenses", systemImage: "minus.circle.fill")
+                }
+            IncomeView(budget: $budget)
+                .tabItem {
+                    Label("Income", systemImage: "plus.circle.fill")
+                }
+            StockView() // New tab for stock information
+                .tabItem {
+                    Label("Stocks", systemImage: "chart.bar.fill")
+                }
+        }
+    }
+}
